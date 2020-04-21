a1fc4da290b44205d5e156904dda3ff540bbfe44
Archaengel <god11341258@gmail.com>
2020-04-20 18:43:24 -0700

    Install basic routing and design deps. Establish dir structure

---
+++ src/sections/Shipments/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Layout } from 'antd';
+import styled from 'styled-components';
+
+const { Content: AntdContent } = Layout;
+
+const Content = styled(AntdContent)``;
+
+export const Shipments = () => {
+  return <Content>Shipments</Content>;
+};
